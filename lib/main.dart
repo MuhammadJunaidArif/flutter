2cf5db4ffb0a46bbe8f65dd19a5acf0ab13f5b2c
MuhammadJunaidArif <junaidarif1099@gmail.com>
2022-06-14 15:51:00 +0500

    day 3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_catalog/home_page.dart';
+import 'package:flutter_catalog/pages/home_page.dart';
+import 'package:flutter_catalog/pages/login_page.dart';
 
 void main(List<String> args) {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: Homepage());
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        themeMode: ThemeMode.light,
+        theme: ThemeData(primarySwatch: Colors.deepPurple),
+        darkTheme: ThemeData(primarySwatch: Colors.red),
+        initialRoute: "/home",
+        routes: {
+          "/": (context) => LoginPage(),
+          "/home": (context) => Homepage(),
+          "/login": (context) => LoginPage(),
+        });
   }
 }
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatelessWidget {
+  final int days = 30;
+  final String name = "junaid";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Catalog App')),
+      body: Center(
+          child: Container(
+        child: Text('Welcom to Flutter $days days course by $name '),
+        //string inter pulation  welcom..... $days...
+      )),
+      drawer: Drawer(),
+    );
+  }
+}
