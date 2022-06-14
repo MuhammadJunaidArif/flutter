a7b44c2d1b15c76159f1ae8bb5bfea6deeacc6ce
MuhammadJunaidArif <junaidarif1099@gmail.com>
2022-06-14 17:04:18 +0500

    day4 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/pages/home_page.dart';
 import 'package:flutter_catalog/pages/login_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main(List<String> args) {
   runApp(MyApp());
 }
 
+// ignore: use_key_in_widget_constructors
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         themeMode: ThemeMode.light,
-        theme: ThemeData(primarySwatch: Colors.deepPurple),
+        theme: ThemeData(
+          primarySwatch: Colors.deepPurple,
+          fontFamily: GoogleFonts.lato().fontFamily,
+          primaryTextTheme: GoogleFonts.latoTextTheme(),
+        ),
         darkTheme: ThemeData(primarySwatch: Colors.red),
-        initialRoute: "/home",
+        // initialRoute: "/home",
         routes: {
           "/": (context) => LoginPage(),
           "/home": (context) => Homepage(),
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,18 +1,59 @@
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-          child: Text(
-        'Login Page',
-        style: TextStyle(
-          fontSize: 20,
-          color: Colors.blue,
-          fontWeight: FontWeight.bold,
-        ),
-      )),
+      color: Colors.white,
+      child: Column(
+        children: [
+          Image.asset("assests/images/login_image.png", fit: BoxFit.cover),
+          // ignore: prefer_const_constructors
+          SizedBox(
+            height: 20,
+          ),
+          // ignore: prefer_const_constructors
+          Text(
+            'Welcome',
+            // ignore: prefer_const_constructors
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          // ignore: prefer_const_constructors
+          SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                      hintText: "Enter User Name....", labelText: "User Name"),
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                      hintText: "Enter Password....", labelText: "Password"),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                  child: Text("Login"),
+                  onPressed: () {
+                    print("hi junaid");
+                  },
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
